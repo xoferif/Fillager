f47d4c32eda78f75046e82c9bcc868735ff1a8be
Jesper baunsgaard <jesper.j.b.baunsgaard@gmail.com>
2017-02-01 11:22:15 +0100

    removed LoginView from index.cshtml & removed unused variables from registration view

--- Fillager/ViewModels/RegisterViewModel.cs
+++ Fillager/ViewModels/RegisterViewModel.cs
@@ -19,10 +19,5 @@ namespace Fillager.ViewModels
         [Required]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-        [Required]
-        public string FullName { get; set; }
-        [Required]
-        [DataType(DataType.Date)]
-        public DateTime BirthDate { get; set; }
     }
 }
