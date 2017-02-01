7bb6a4a6f1cf7f77de3cc473178416f576b7827b
Morten Skov Olsen <sniffern@msn.com>
2017-02-01 15:21:02 +0100

    small corrections to layout

--- Fillager/Controllers/AccountController.cs
+++ Fillager/Controllers/AccountController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -89,7 +85,7 @@ namespace Fillager.Controllers
 
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Fillager");
                 }
 
                 ModelState.AddModelError("", "Invalid login!");
