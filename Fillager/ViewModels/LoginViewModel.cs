37c6668f8da89982beac3196b6294ea8ccab582c
Jesper baunsgaard <jesper.j.b.baunsgaard@gmail.com>
2017-02-07 08:24:37 +0100

    fixed login & registration validation and moved login into its own view

--- Fillager/ViewModels/LoginViewModel.cs
+++ Fillager/ViewModels/LoginViewModel.cs
@@ -5,13 +5,16 @@ namespace Fillager.ViewModels
     public class LoginViewModel
     {
         [Required]
+        [Display(Name = "Username")]
         public string UserName { get; set; }
 
         [Required]
+        [Display(Name = "Password")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required]
+        [Display(Name = "Remember me")]
         public bool RememberMe { get; set; }
     }
 }
