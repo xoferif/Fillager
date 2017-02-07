734b7365f8f6a823bea8eaeadd38e08b7f78f197
Jesper baunsgaard <jesper.j.b.baunsgaard@gmail.com>
2017-02-07 09:54:05 +0100

    added a FileList View Component for both private and public files

---
+++ Fillager/ViewComponents/FileListViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Fillager.Models.Files;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fillager.ViewComponents
+{
+    public class FileListViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(IEnumerable<File> files, bool editable)
+        {
+            ViewBag.Editable = editable;
+            return View(files);
+        }
+    }
+}
